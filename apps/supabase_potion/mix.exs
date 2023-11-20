f3821bcdc63bcac98abbdd2d82918bb8f53491cd
Zoey de Souza Pessanha <zoey.spessanha@zeetech.io>
2023-11-20 16:45:33 -0300

    feat: publish go_true package

--- apps/supabase_potion/mix.exs
+++ apps/supabase_potion/mix.exs
@@ -32,7 +32,7 @@ defmodule Supabase.Potion.MixProject do
       {:finch, "~> 0.16"},
       {:jason, "~> 1.4"},
       {:ecto, "~> 3.10"},
-      {:ex_doc, ">= 0.0.0", runtime: false, only: [:dev]},
+      {:ex_doc, ">= 0.0.0", runtime: false, only: [:dev, :prod]},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.3", only: [:dev], runtime: false}
     ]
